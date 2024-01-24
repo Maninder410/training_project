4543b2c84b9c2013a1f6f71b6a0b19b3aca5a126
Maninder Singh <maninder.singh@adw.kochartech.com>
2024-01-24 13:48:33 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { useState } from 'react'
+import Login from './components/Login';
+function App() {
+  const [inc,setInc] = useState(0);
+
+ 
+
+
+  return (
+    <>
+     <Login/>
+
+    </>
+  )
+}
+
+export default App
